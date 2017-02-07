a388fcc05fca031c6e375e443e9b9c6c09c643b4
Claire <email": null>
2017-02-07 10:37:00 -0800

    make errors appear on the sign up page if important information is needed

--- pomodoro-timer/app/controllers/users_controller.rb
+++ pomodoro-timer/app/controllers/users_controller.rb
@@ -14,10 +14,11 @@ class UsersController < ApplicationController
     p user_params
     @user.password = user_params[:password_digest]
     p @user
-      if @user.save!
+      if @user.save
         session[:user_id] = @user.id
         render :index
       else
+       p @user.errors.full_messages
         render :new
       end
   end
