f68deafb2f9654660b9bdca6b9b0e7df54492eb2
Claire <email": null>
2017-02-08 09:42:09 -0800

    add destroy controller to login controller

--- pomodoro-timer/app/controllers/login_controller.rb
+++ pomodoro-timer/app/controllers/login_controller.rb
@@ -6,11 +6,15 @@ class LoginController < ApplicationController
   def create
     @user = User.find_by(email: params[:login][:email])
     if @user && @user.authenticate(params[:login][:password])
-      p @user
+      session[:current_user_id] = @user.id
+      redirect_to root_url
+    else
+      redirect_to new_login_path
     end
-    redirect_to new_login_path
   end
 
   def destroy
+    @current_user = session[:current_user_id] = nil
+    redirect_to root_url
   end
 end
