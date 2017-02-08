7b15983c43c98cd05f799c0a89ad48a9319edb77
Claire <email": null>
2017-02-08 11:46:09 -0800

    edits login create and login destroy in order to create and destroy sessions

--- pomodoro-timer/app/controllers/login_controller.rb
+++ pomodoro-timer/app/controllers/login_controller.rb
@@ -1,14 +1,18 @@
 class LoginController < ApplicationController
+
+  #the redirects on this page will go to the homepage (the ones going to root_url)
   def new
     render :new
   end
 
   def create
     @user = User.find_by(email: params[:login][:email])
-    if @user && @user.authenticate(params[:login][:password])
+    p params
+     p params[:login][:password_digest]
+    if @user && @user.authenticate(params[:login][:password_digest])
       session[:current_user_id] = @user.id
        p session[:current_user_id]
-      redirect_to user_index_path
+      redirect_to root_url
     else
       redirect_to new_login_path
     end
@@ -18,6 +22,7 @@ class LoginController < ApplicationController
     p "current user #{current_user}"
     current_user = session[:current_user_id] = nil
     p current_user
-    redirect_to new_login_path
+    redirect_to root_url
   end
+
 end
--- pomodoro-timer/config/routes.rb
+++ pomodoro-timer/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :login, :only => [:create, :new, :destroy]
 
   #Need to add route to home page here
-
+  root 'users#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
