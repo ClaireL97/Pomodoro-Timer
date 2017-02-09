4401b8340dd44fcd4baa63c972f2bd68a907b96d
Austin Kading <email": null>
2017-02-08 18:01:43 -0800

    adding tasks (work in progress)

--- pomodoro-timer/app/controllers/tasks_controller.rb
+++ pomodoro-timer/app/controllers/tasks_controller.rb
@@ -1,5 +1,29 @@
 class TasksController < ApplicationController
   def show
-    render :show
+    p "------------------------------------------------------------"
+    p params
+    p "------------------------------------------------------------"
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    p "@@@@@@@@@@@@@@@@@@@@"
+    @task = Task.new(params[:submitted_task])
+    # p params
+    if @task.save!
+      redirect_to task_path(@task.id)
+    else
+      redirect_to new_task_path
+    end
+
+  end
+
+  def new
+    @task = Task.new
+    render :new
+  end
+
+  def index
+    render :index
   end
 end
--- pomodoro-timer/app/models/user.rb
+++ pomodoro-timer/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-
+  has_many :tasks
   validates :username, :password_digest, :email, presence: true
   validates :username, :email, uniqueness: true
 end
