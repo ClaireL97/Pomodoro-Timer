788e490510e74a6df4d89fb83b17f41ed13a9029
Austin Kading <email": null>
2017-02-09 12:14:55 -0800

    Refactors

--- pomodoro-timer/app/controllers/application_controller.rb
+++ pomodoro-timer/app/controllers/application_controller.rb
@@ -1,12 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  private
-
-  def current_user
-    @current_user ||= session_user.id &&
-      User.find_by(id: session_user.id)
-  end
-
 end
 
