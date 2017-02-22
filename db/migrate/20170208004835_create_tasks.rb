d68ee4905c7f62703e447a06fa6d9164a829cecb
Claire <email": null>
2017-02-22 14:17:47 -0800

    removes extra space in validation

--- db/migrate/20170208004835_create_tasks.rb
+++ db/migrate/20170208004835_create_tasks.rb
@@ -1,7 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[5.0]
   def change
     create_table :tasks do |t|
-      t.string :submitted_task
+      t.string :submitted_task, null: false
       t.integer :user_id
       t.timestamps
     end
